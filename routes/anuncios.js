c613bbaa8c331d77d4ad072f05ac9176b1a71dbf
Eric Risco de la Torre <erisco@icloud.com>
2016-10-23 13:20:56 +0200

    Pràctica acabada, però es pot millorar

--- routes/anuncios.js
+++ routes/anuncios.js
@@ -26,36 +26,60 @@ router.get('/', function(req, res, next){
 	var sort = req.query.sort || null;
 	var limit = parseInt(req.query.limit) || null;
 	var skip = parseInt(req.query.skip) || 0;
+  	var fields = req.query.fields || null;
 
 	//crear filtro
 	var filter = {};
-	if(typeof venta !== 'undefined') filter.venta = venta;
+	//Si el querystring contiene la variable venta agregar al filtro y comprobar que sea true o false
+	if(typeof venta !== 'undefined' && (venta == 'true' || venta == 'false')) {
+		filter.venta = venta;
+	}
 
+	//Si el querystring contiene las variable lte (lower than or equal) y gte (greater than or equal)
 	if(typeof lte !== 'undefined' && typeof gte !== 'undefined') {
+		//si contiene las dos variables agregarlas al filtro
 		filter.precio = { $lte: lte, $gte: gte };
-	}else if(typeof gte !== 'undefined'){
+	}else if(typeof lte === 'undefined' && typeof gte !== 'undefined'){
+		//de otro modo si solo contiene gte agregarlo al filtro
 		filter.precio = { $gte: gte };
-	}else if(typeof lte !== 'undefined'){
+	}else if(typeof lte !== 'undefined' && typeof gte === 'undefined'){
+		//de otro modo si solo contiene lte agregarlo al filtro
 		filter.precio = { $lte: lte };
 	}
 
+	//Si el querystring contiene la variable tag
 	if(typeof tags !== 'undefined'){
-		if(typeof tags !== Array){
-			console.log(typeof tags);
+		//En este caso, he contemplado que se pueda entrar solo un tag o varios.
+		if(typeof tags == 'string'){
+			//En el caso de que solo se entre un tag, se crea una lista a partir de el y creamos el filtro con $in
 			var tagList = [];
 			tagList.push(tags);
 			filter.tags = { "$in" : tagList }
 		}else{
+			//En el caso de que se entren varios tags, le damos a $in todo los valores
 			filter.tags = { "$in" : tags }
 		}
 	}
 
-	console.log(filter);
+	//Buscamos los resultados con todos los parametros de entrada creando la query
+    var query = Anuncio.find(filter);
+    query.sort(sort);
+    query.limit(limit);
+    query.skip(skip);
 
-	Anuncio.find(filter,sort, function(err,anuncios){
-		if(err) return next(err);
+    //En el caso de que se quiera hacer un select de mas de un campo se requiere una transformación a string separados
+    //por espacios 
+    if(fields != null){
+    	fields = fields.toString().replace(","," ");
+    }
+    query.select(fields);
+
+    query.exec(function(err, anuncios) {
+		if(err) return next(err); // return si hay algun error
+
+		//Si todo ha ido bien. También devolvemos la carpeta donde estan las imagenes :)
 		res.json({success:true, images_folder: config.images_folder, anuncios: anuncios});
-	});
+    });
 
 });
 
